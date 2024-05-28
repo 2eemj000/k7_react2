1667b2f87daae8ef76029800eccb57f825698b36
2eemj000 <2eemj000@gmail.com>
2024-05-28 17:59:21 +0900

    0528 수정

--- src/01/login.js
+++ src/01/login.js
@@ -20,7 +20,7 @@ export default function Login() {
             <div class="flex items-center justify-between">
               <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
               <div class="text-sm">
-                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                <a href="#" class="font-semibold text-blue-600 hover:text-blue-500">Forgot password?</a>
               </div>
             </div>
             <div class="mt-2">
@@ -29,7 +29,7 @@ export default function Login() {
           </div>
 
           <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
           </div>
         </form>
       </div>
