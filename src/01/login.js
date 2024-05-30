be3783c785738805a37ce3df889c0fd06f2142be
2eemj000 <2eemj000@gmail.com>
2024-05-30 14:05:47 +0900

    0530 수정

--- src/01/login.js
+++ src/01/login.js
@@ -51,12 +51,17 @@ return (
                         <label htmlFor="password" className="block text-sm font-medium leading-6 text-slate-600">Password</label>
                     </div>
                     <div className="mt-2">
-                        <input ref={txt2Ref} id="password" name="password" type="text"
+                        <input ref={txt2Ref} id="password" name="password" type="password"
                             required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
                 <div>
-                    <button onClick={handleOK} type="submit" className="flex w-full justify-center rounded-md bg-orange-400 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500">Sign in</button>
+                    {loginTF === true ? (
+                    <button onClick={handleOK} type="submit" className="flex w-full justify-center rounded-md bg-orange-400 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500">sign out</button>
+                    ) : (
+                        <button onClick={handleOK} type="submit" className="flex w-full justify-center rounded-md bg-orange-400 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500">sign in</button>
+                    )}
+                    
                 </div>
             </form>
         </div>
